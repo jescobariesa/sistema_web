ea969b1c6fdce073d55757256525c640805d06ba
geovannyEs <jescobariesa@gmail.com>
2025-08-12 00:00:07 -0600

    Intengo de rutas

--- src/app/modules/forms-registro/forms-registro.ts
+++ src/app/modules/forms-registro/forms-registro.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'forms-registro',
-  imports: [],
   templateUrl: './forms-registro.html',
   styleUrl: './forms-registro.css'
 })
