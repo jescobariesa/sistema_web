774bfa5010eaa11fe208c8d675d0ed73fbd6fa44
geovannyEs <jescobariesa@gmail.com>
2025-09-07 11:35:24 -0600

    Creación de suppliers y se agregan a rutas 20250907

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,41 +7,60 @@ import { GestionUsuariosCommponent } from './modules/gestion-usuarios/gestion-us
 import { UserPendingCommponent } from './modules/user_pending/user-pending';
 import { ArticulosNuevosCommponent } from './modules/home/sub-modules/articulos-nuevos/articulos-nuevos';
 import { authGuard } from './guards/auth.guard';
+import { SuppliersComponent } from './modules/home/sub-modules/suppliers/suppliers';
 
 export const routes: Routes = [
 
   // 🔓 Rutas públicas (sin sesión)
-  { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'registro', component: FormsRegistroComponent },
+  { path: '',
+    component: LoginComponent
+  },
+
+  { path: 'login',
+    component: LoginComponent
+  },
+
+  { path: 'registro',
+    component: FormsRegistroComponent
+  },
 
   // 🔒 Rutas protegidas (requieren login y estado = activo)
-  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'home',
+    component: HomeComponent,
+    canActivate: [authGuard]
+  },
 
-  // Solo admin
+  // Accede el rol de: administrador
   { path: 'rol',
     component: RolesCommponent,
     canActivate: [authGuard],
     data: {roles: ['rol_admin'] }
   },
+
   { path: 'user',
     component: GestionUsuariosCommponent,
     canActivate: [authGuard],
     data: {roles: ['rol_admin'] }
   },
 
-  // Solo admin (ejemplo: ver usuarios pendientes)
   { path: 'userp',
     component: UserPendingCommponent,
     canActivate: [authGuard],
     data: { roles: ['rol_admin'] }
   },
 
-  // Solo empleados (ejemplo: artículos nuevos en Home)
+  // Accede el rol de: administrador, empleados
   { path: 'new-art',
     component: ArticulosNuevosCommponent,
     canActivate: [authGuard],
     data: { roles: ['rol_admin', 'rol_empleados'] }
+  },
+
+  // Accede el rol de: administrador, empleados
+  { path: 'provid',
+    component: SuppliersComponent,
+    canActivate: [authGuard],
+    data: { roles: ['rol_admin', 'rol_empleados']}
   },
 
   // Ruta por defecto
