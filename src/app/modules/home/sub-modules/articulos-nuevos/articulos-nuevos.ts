1b4308ccc297087076afa647e140e7e87977f364
ddiaziesa <ddiaziesa@gmail.com>
2025-09-07 09:44:01 -0600

    tabla articulos

--- src/app/modules/home/sub-modules/articulos-nuevos/articulos-nuevos.ts
+++ src/app/modules/home/sub-modules/articulos-nuevos/articulos-nuevos.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-articulos-nuevos',
-  imports: [RouterModule],
+  imports: [RouterModule,MatIconModule],
   templateUrl: './articulos-nuevos.html',
   styleUrl: './articulos-nuevos.css'
 })
