2e742840f085a82cf2cdd48372e8ee2e6f2f5772
ddiaziesa <ddiaziesa@gmail.com>
2025-09-14 16:09:32 -0600

    Proveedores y modificacion

--- src/app/modules/userdenied/userdenied.ts
+++ src/app/modules/userdenied/userdenied.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { SessionService } from '../../services/session.service';
 
 @Component({
   selector: 'app-userdenied',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './userdenied.html',
   styleUrl: './userdenied.css'
 })
 export class UserdeniedComponent {
 
+
+ // Inyectamos varios servicios en un constructor para no alterar el funcionamiento
+  constructor(
+    private sessionService: SessionService,
+    private router: Router
+  ) {}
+
+onLogout() {
+    this.sessionService.logout();
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/modules/userinactivate/userinactivate.ts
+++ src/app/modules/userinactivate/userinactivate.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { SessionService } from '../../services/session.service';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-userinactivate',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './userinactivate.html',
   styleUrl: './userinactivate.css'
 })
 export class UserinactivateComponent {
+
+    // Inyectamos varios servicios en un constructor para no alterar el funcionamiento
+  constructor(
+    private sessionService: SessionService,
+    private router: Router
+  ) {}
+
+onLogout() {
+    this.sessionService.logout();
+    this.router.navigate(['/login']);
+  }
 
 }
